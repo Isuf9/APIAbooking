0f8bd239f54d6448263e53be2277b044445eee3b
Arber Dresha <abi_kampioni@hotmail.com>
2020-07-17 17:53:50 +0200

    Add unit testing

--- APIAbooking/Models/APIAbookingContext.cs
+++ APIAbooking/Models/APIAbookingContext.cs
@@ -32,7 +32,7 @@ namespace APIAbooking.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=.; Database=APIAbooking_II; user id=sa; password=isufisuf");
+                optionsBuilder.UseSqlServer("Server=Tornado\\Arber; Database=APIAbooking_II; user id=sa; password=100200300");
             }
         }
 
